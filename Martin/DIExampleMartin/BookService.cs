0d92be535829553c3b3d92b3fdef31c2bc34a73b
Martin <martin@kuenz.co.at>
2018-09-10 17:04:58 +0200

    1st commit

---
+++ Martin/DIExampleMartin/BookService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIExampleMartin
+{
+    public class BookService : IBookService
+    {
+        public string Book(string name) => $"Buchname, {name}";
+    }
+}
---
+++ Martin/DIExampleMartin/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DIExampleMartin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variante 1 ohne AppService
+            //var controller = new BooksController(new BookService());
+
+            // Variante 2 mit AppService
+            var controller = AppService.Instance.Container.GetService<BooksController>();
+
+            string book = controller.Book("Das ist mein erstes Buch");
+            Console.WriteLine(book);
+        }
+    }
+}
