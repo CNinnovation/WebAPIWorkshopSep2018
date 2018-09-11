b66345c13b14402e10b9d996d88b77ba12266495
Christian Nagel <christian@christiannagel.com>
2018-09-11 16:31:33 +0200

    authentication sample

---
+++ AuthenticationSample/AuthenticationSample/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AuthenticationSample.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AuthenticationSample.Areas.Identity.IdentityHostingStartup))]
+namespace AuthenticationSample.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ AuthenticationSample/AuthenticationSample/Controllers/MyDemoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationSample.Controllers
+{
+    [Authorize]
+    public class MyDemoController : Controller
+    {
+        public string Index()
+        {
+            string user = User.Identity.Name;
+            return user;
+        }
+
+        [AllowAnonymous]
+        public string Allow()
+        {
+            return "allow all";
+        }
+    }
+}
